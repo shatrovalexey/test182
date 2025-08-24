e7a82d5f7a80a53b97f7766a9dcdade735cfc98c
Алексей <mail@ashatrov.ru>
2025-08-24 07:03:02 +0300

    Initial

--- Решения/Решение 4/run.pl
+++ Решения/Решение 4/run.pl
@@ -5,8 +5,7 @@ use DBI;
 my $dbh = DBI->connect(@::ENV{+qw{DB_DSN DB_USER DB_PASSWD}});
 my ($size, $offset) = (1000, 0);
 
-do {
-	my $sth_sel = $dbh->prepare(<< ".");
+$offset += $size while @{$dbh->selectall_arrayref(<< ".")}
 SELECT
 	`t1`.`id`
 	, `t1`.`data`
@@ -14,12 +13,3 @@ FROM
 	`test` AS `t1`
 LIMIT $size OFFSET $offset;
 .
-	$sth_sel->execute;
-
-	last + ( ) unless $sth_sel->rows > 0;
-
-	undef while $sth_sel->fetchrow_array;
-
-	$sth_sel->finish;
-	$offset += $size;
-} until undef
