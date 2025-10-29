12382ff64933f808a2780e54ea0f8b64a95a3529
Алексей <mail@ashatrov.ru>
2025-10-29 08:14:32 +0300

    Done

--- Решения/Решение 1/run.pl
+++ Решения/Решение 1/run.pl
@@ -1,7 +1,6 @@
 use strict;
 use warnings;
 use Data::Dumper;
-use Digest::SHA1 qw{sha1_hex};
 
 sub getRemoveDuplicates($) {
 	my ($hash) = @_;
@@ -9,12 +8,12 @@ sub getRemoveDuplicates($) {
 
 	reset %$hash;
 
-	while (my($key, $value) = each %$hash) {
-		my $key2 = &sha1_hex($value);
-
-		delete $hash->{$key} if exists $keys{$key2};
-
-		$keys{$key2} = undef
+	while (my($key, $key2) = each %$hash) {
+		if (exists $keys{$key2}) {
+			delete $hash->{$key}
+		} else {
+			$keys{$key2} = undef
+		}
 	}
 
 	$hash
